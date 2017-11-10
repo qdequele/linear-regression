ece35efa9a9ffc6a04423ceb44f1c436f85b4176
qdequele <quentin@dequelen.me>
2017-11-10 11:24:44 +0100

    continue working on

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use std::string::*;
 
 mod resolver;
 
+use resolver::{SetValue};
+
 fn main() {
 	
 	let args: Vec<String> = env::args().collect();
@@ -37,6 +39,7 @@ fn main() {
 	};
 
 	let f = BufReader::new(f);
+	let mut _set : Vec<SetValue> = Vec::new();
 
 	for line in f.lines() {
 		let line = match line {
@@ -46,9 +49,23 @@ fn main() {
 				return
 			},
 		};
-		println!("Line: {}", line);
+		let tab : Vec<&str> = line.split(",").collect();
+		let _x : &str = tab[0];
+		let _y : &str = tab[1];
+
+		if _x.parse::<f64>().is_ok() && _y.parse::<f64>().is_ok() {
+			let value : SetValue = SetValue {
+				x: _x.parse().unwrap(),
+				y: _y.parse().unwrap()
+			};
+			_set.push(value);
+		}
 	}
 
-	let mut resolver = resolver::Resolver::new();
+	println!("{:?}", _set);
+
+	let mut resolver = resolver::Resolver::new(_set);
 	resolver.learn();
+
+	println!("Ø0 = {:?} | Ø1 = {:?}", resolver.theta0, resolver.theta1);
 }
--- src/resolver.rs
+++ src/resolver.rs
@@ -4,65 +4,68 @@
  * Author: Quentin de Quelen (quentin@dequelen.me)
  */
 
+#[derive(Debug)]
 pub struct SetValue {
-	x: f64,
-	y: f64,
+	pub x: f64,
+	pub y: f64,
 }
 
 pub struct Resolver {
-	// theta0: f64, // Ø0
-	// theta1: f64, // Ø1
+	pub theta0: f64, // Ø0
+	pub theta1: f64, // Ø1
 	delta_theta: f64, // DØ = 1000.0
 	learning_rate: f64, // alpha = 2.0
 	number_of_training: u64, // m = 100
-	// tmp_theta0: f64,
-	// tmp_theta1: f64,
-	set: Vec<SetValue>,
+	pub set: Vec<SetValue>,
 }
 
 impl Resolver {
 
 	#[allow(dead_code)]
 	pub fn learn(&mut self) {
-		self.calculate_gradient_descent_minimization();
+		self.training_loop();
 	}
 
 	#[allow(dead_code)]
-	pub fn new() -> Resolver {
+	pub fn new(set : Vec<SetValue>) -> Resolver {
 		Resolver {
-			delta_theta: 3.0,
-			learning_rate: 0.1,
-			number_of_training: 1_000
+			theta0: 0_f64,
+			theta1: 0_f64,
+			delta_theta: 3_f64,
+			learning_rate: 0.1_f64,
+			number_of_training: 1_000_u64,
+			set: set
 		}
 	}
 
-	pub fn hypothesis(&mut self, x: f64) -> f64 {
-		return self.theta0 + (self.theta1 * x)
+	pub fn hypothesis(&self, x: f64) -> f64 {
+		self.theta0 + (self.theta1 * x)
 	}
-
-	fn train(&mut self) -> (f64, f64) {
-
+	
+	fn train(&self, set: &Vec<SetValue>) -> (f64, f64) {
 		let m = self.set.len() as f64;
 		let mut sum_0 = 0_f64;
 		let mut sum_1 = 0_f64;
 
-		for val in self.set {
+		for val in set {
 			let d = self.hypothesis(val.x) * val.y;
 			sum_0 += d;
 			sum_1 += d * val.x;
 		}
 
-		(self.learning_rate * ( 1_f64 / m) * sum_0, self.learning_rate * ( 1_f64 / m) * sum_1);
+		(self.learning_rate * ( 1_f64 / m) * sum_0, self.learning_rate * ( 1_f64 / m) * sum_1)
 	}
 
+	#[allow(dead_code)]
 	fn training_loop(&mut self) {
 		for _ in 0..self.number_of_training {
-			let (tmp_theta0, tmp_theta1) = self.train();
+			let (tmp_theta0, tmp_theta1) = self.train(&self.set);
 			self.theta0 -= tmp_theta0;
 			self.theta1 -= tmp_theta1;
 		}
 	}
 
+	#[allow(dead_code)]
 	fn calculate_gradient_descent_minimization(&mut self) {
 		for x in 0..self.number_of_training {
 			println!("{} \t| {} \t| {}", x, self.delta_theta, (self.learning_rate * 2.0 * self.delta_theta));
